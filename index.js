e04aed80eb1766dcdf49f86b091911c788be799a
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2017-06-15 11:38:38 +0200

    Make sure it also works when included as an npm module
    When I uploaded the package to npm, the index.js in the package was
    itself placed under node_modules, which made the exclude-node-modules
    feature not work at all (so 'use strict' would be applied to all files.
    
    Now we use a substring of __dirname up to the first occurrence of the
    string 'node_modules' as the base directory name.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var module = require('module')
 var compile = module.prototype._compile
 var path = require('path')
 
+var index = __dirname.indexOf('node_modules')
+var baseDirectory = (index < 1) ? __dirname : __dirname.substr(0, index - 1)
+
 module.prototype._compile = function (content, filename) {
-  var isThirdParty = filename.startsWith(path.join(__dirname, 'node_modules'))
+  var isThirdParty = filename.startsWith(path.join(baseDirectory, 'node_modules'))
   if (!isThirdParty) {
     content = "'use strict';" + content
   }
