6aea841e077edf10dd2a0d401c5de2421b71891d
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2017-06-19 23:44:17 +0200

    Refactor out the path concatenation so it doesn't happen every time

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var compile = module.prototype._compile
 var path = require('path')
 
 var index = __dirname.indexOf('node_modules')
-var baseDirectory = (index < 1) ? __dirname : __dirname.substr(0, index - 1)
+var nodeModulesDir = (index < 1) ? path.join(__dirname, 'node_modules') : __dirname.substr(0, index + 12)
 
 module.prototype._compile = function (content, filename) {
-  var isThirdParty = filename.startsWith(path.join(baseDirectory, 'node_modules'))
+  var isThirdParty = filename.startsWith(nodeModulesDir)
   if (!isThirdParty) {
     content = "'use strict';" + content
   }
